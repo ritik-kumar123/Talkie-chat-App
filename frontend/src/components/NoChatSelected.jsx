49237be216d00c39856b05140c39df8432fcc25a
ritik kumar <148605032+ritik-kumar123@users.noreply.github.com>
2025-08-21 09:01:51 +0530

    NOChatSelected component  build

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -2,7 +2,24 @@ import { MessageSquare } from "lucide-react";
 
 const NoChatSelected = () => {
   return <>
-  <div></div>
+  <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-white/50">
+      <div className="max-w-md text-center space-y-6">
+          {/* ICON Display */}
+            <div className="flex justify-center gap-4 mb-4">
+                <div className="relative">
+                  <div className="w-16 h-16 rounded-2xl bg-blue-100 flex items-center 
+                    justify-center animate-bounce">
+                      <MessageSquare className="w-8 h-8 text-blue-600"/>
+                  </div>
+                </div>
+            </div>
+            {/* Welcome Text */}
+            <h2 className="text-2xl font-bold text-gray-800">Welcome to Talkle!</h2>
+            <p className="text-gray-500">
+               Select a conversation from the sidebar to start chatting
+            </p>
+      </div>
+  </div>
   </>;
 };
 
